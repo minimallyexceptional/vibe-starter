abb35469709346527d077e4e5e123828f0fb4e79
Minimally Exceptional <minimallyexceptionalapps@gmail.com>
2025-09-21 10:40:31 -0400

    feat: add stripe billing checkout to account

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,12 @@
 
 interface ImportMetaEnv {
   readonly VITE_CLERK_PUBLISHABLE_KEY?: string
+  readonly VITE_STRIPE_PUBLISHABLE_KEY?: string
+  readonly VITE_STRIPE_PRICE_ID_STARTER?: string
+  readonly VITE_STRIPE_PRICE_ID_GROWTH?: string
+  readonly VITE_STRIPE_PRICE_ID_SCALE?: string
+  readonly VITE_STRIPE_SUCCESS_URL?: string
+  readonly VITE_STRIPE_CANCEL_URL?: string
 }
 
 interface ImportMeta {
